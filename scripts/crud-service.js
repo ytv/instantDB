de79066f53a72da97f4a7e2753fff3b9c23d28ac
ytv <sillying@gmail.com>
2016-08-02 18:07:33 -0700

    Initial Commit
    Simplified services by removing replacement/find/update

--- scripts/crud-service.js
+++ scripts/crud-service.js
@@ -37,42 +37,6 @@ module.exports = {
         })
     },
 
-    replace: function(url, collectionName, query, replacement) {
-        mongo.connect(url, function(err, db) {
-            if(err) throw "ERROR connecting to DB - " + err;
-            db.collection(collectionName).update(query, replacement, function(err, data) {
-                if(err)
-                    throw "ERROR replacing document - " + err;
-                console.log('Document successfully replaced in \'' + collectionName + '\'');
-                db.close();
-            });
-        });
-    },
-
-    update: function(url, collectionName, query, updatedData) {
-        mongo.connect(url, function(err, db) {
-            if(err) throw "ERROR connecting to DB - " + err;
-            db.collection(collectionName).update(query, {$set: updatedData}, function(err, data) {
-                if(err)
-                    throw "ERROR updating document - " + err;
-                console.log('Document successfully updated in \'' + collectionName + '\'');
-                db.close();
-            });
-        });
-    },
-
-    delete: function(url, collectionName, query) {
-        mongo.connect(url, function(err, db) {
-            if(err) throw "ERROR connecting to DB - " + err;
-            db.collection(collectionName).remove(query, function(err, data) {
-                if(err)
-                    throw "ERROR deleting document - " + err;
-                console.log('Document successfully deleted in \'' + collectionName + '\'');
-                db.close();
-            })
-        });
-    },
-
     deleteAll: function(url, collectionName) {
         mongo.connect(url, function(err, db) {
             if(err) throw "ERROR connecting to DB - " + err;
@@ -83,5 +47,41 @@ module.exports = {
                 db.close();
             });
         });
-        }
+    }
+    // replace: function(url, collectionName, query, replacement) {
+    //     mongo.connect(url, function(err, db) {
+    //         if(err) throw "ERROR connecting to DB - " + err;
+    //         db.collection(collectionName).update(query, replacement, function(err, data) {
+    //             if(err)
+    //                 throw "ERROR replacing document - " + err;
+    //             console.log('Document successfully replaced in \'' + collectionName + '\'');
+    //             db.close();
+    //         });
+    //     });
+    // },
+    //
+    // update: function(url, collectionName, query, updatedData) {
+    //     mongo.connect(url, function(err, db) {
+    //         if(err) throw "ERROR connecting to DB - " + err;
+    //         db.collection(collectionName).update(query, {$set: updatedData}, function(err, data) {
+    //             if(err)
+    //                 throw "ERROR updating document - " + err;
+    //             console.log('Document successfully updated in \'' + collectionName + '\'');
+    //             db.close();
+    //         });
+    //     });
+    // },
+    //
+    // delete: function(url, collectionName, query) {
+    //     mongo.connect(url, function(err, db) {
+    //         if(err) throw "ERROR connecting to DB - " + err;
+    //         db.collection(collectionName).remove(query, function(err, data) {
+    //             if(err)
+    //                 throw "ERROR deleting document - " + err;
+    //             console.log('Document successfully deleted in \'' + collectionName + '\'');
+    //             db.close();
+    //         })
+    //     });
+    // },
+
 }
