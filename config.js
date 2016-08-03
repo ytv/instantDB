e4690b9a512f9ec76c697b73f39c4b3fab97c2d4
ytv <sillying@gmail.com>
2016-08-02 18:05:16 -0700

    Initial Commit Moved config files into one folder

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.db = {};
-
-config.db.url = 'mongodb://localhost/test';
-// config.db.url = 'mongodb://admin:password@ds017248.mlab.com:17248/mockdb';
-
-module.exports = config;
